382e4fc8da2f1fb5e61987f76d3b6f49a609b3f3
kudaibergenov_b@auca.kg <kudaibergenov_b@auca.kg>
2021-09-27 14:52:34 +0600

    uri-midterm URI1003 compilation error

---
+++ uri-midterm/src/URI1003.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        int s = a + b;
+
+        System.out.println("SOMA = " + s);
+    }
+}
