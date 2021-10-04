4254e63c28b7ada8e7f9f78a2479966bfc7c0b44
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-10-04 15:10:44 +0600

    URI 1001 Accepted

---
+++ uri-midterm/src/URI1001.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class URI1001 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int a = inp.nextInt();
+
+        int b = inp.nextInt();
+
+        int s = a + b;
+                System.out.println("X = " + s);
+    }
+}
