3da097c08acd792a0ac20c7f3b6baea7650f3765
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-10-10 21:40:06 +0600

    lab-01 Problem08 completed

---
+++ lab-01/src/Problem08.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int sum = a + b + c + d + e + f + g + h + i;
+        System.out.println(a + " + " + b + " + " + c + " + " + d  + " + " + e + " + " + f + " + " + g + " + " + h + " + "+ i + " = " + sum);
+    }
+}
