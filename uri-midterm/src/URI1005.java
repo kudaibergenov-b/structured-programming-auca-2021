55c26365ef419e7a6b9e7d6587f0f551d1d5cda0
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-10-14 19:36:24 +0600

    URI 1006 Accepted

--- uri-midterm/src/URI1005.java
+++ uri-midterm/src/URI1005.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class URI1005 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
+
         double a = inp.nextDouble();
+
         double b = inp.nextDouble();
+
         double c = ((a * 3.5) + (b *7.5)) / 11;
+
         System.out.printf("MEDIA = %.5f%n",c);
     }
 }
