5d82be4e6b21b97451e6907408c5c8de7518ed97
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-10-18 15:22:54 +0600

    uri-midterm URI1012 Presentation error

---
+++ uri-midterm/src/URI1012.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class URI1012 {
+    public static final double PI = 3.14159;
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double a = inp.nextDouble();
+        double b = inp.nextDouble();
+        double c = inp.nextDouble();
+
+        double A = a * c / 2;
+        double B = PI * c *c;
+        double C = (a + b) / 2 * c;
+        double D = b * b;
+        double E = a * b;
+
+        System.out.printf("TRIANGULO: %.3f%n ", A);
+        System.out.printf("CIRCULO: %.3f%n ", B);
+        System.out.printf("TRAPEZIO: %.3f%n ", C);
+        System.out.printf("QUADRADO: %.3f%n ", D);
+        System.out.printf("RETANGULO: %.3f%n ", E);
+    }
+}
