13dcf9624889ffeb72e37ac52f9b35682298fd53
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-10-23 20:14:43 +0600

    uri-midterm URI1016 Accepted

---
+++ uri-midterm/src/URI1016.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int km = inp.nextInt();
+
+        int minutes = km * 2;
+
+        System.out.println(minutes + " minutos");
+    }
+}
