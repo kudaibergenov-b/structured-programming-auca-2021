d5acd70428306860a27f131bb73e24e0a4592d1e
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-06 16:34:57 +0600

    uri-midterm 1041 Wrong answer

--- uri-midterm/src/URI1041.java
+++ uri-midterm/src/URI1041.java
@@ -23,8 +23,9 @@ public class URI1041 {
             System.out.println("Origem");
         }
         if (x == 0 && y > 0 || x == 0 && y < 0) {
-            System.out.println("Eixo X");
-        } else if (y == 0 && x > 0 || y == 0 && x < 0) {
+            System.out.println("Eixo Y");
+        }
+        if (y == 0 && x > 0 || y == 0 && x < 0) {
             System.out.println("Eixo Y");
         }
     }
