7a850b0ae24a002d21b8deb2ebfdd1302b5b811b
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-06 16:20:18 +0600

    uri-midterm 1041 Wrong answer

---
+++ uri-midterm/src/URI1041.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class URI1041 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double x = inp.nextDouble();
+        double y = inp.nextDouble();
+
+        if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        }
+        if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        }
+        if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        }
+        if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        }
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        }
+        if (x == 0 && y > 0 || x == 0 && y < 0) {
+            System.out.println("Eixo X");
+        } else if (y == 0 && x > 0 || y == 0 && x < 0)
+            System.out.println("Eixo Y");
+    }
+}
