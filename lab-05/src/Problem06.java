19477af6efba33122502d06cfde64b2968cf4c56
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-27 19:46:06 +0600

    lab-05 Problem06 Completed

---
+++ lab-05/src/Problem06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem06 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int data;
+        int sum = 0;
+
+        do {
+            System.out.print("Enter an integer (the input ends if it is 0): ");
+            data = inp.nextInt();
+
+            sum += data;
+        } while (data != 0);
+            System.out.println("The sum is " + sum);
+    }
+}
