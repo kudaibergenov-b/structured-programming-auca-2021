a098216b9df483a265a155d90046d4543a1073c5
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-28 22:15:12 +0600

    uri-final URI1098 Accepted

---
+++ uri-final/src/URI1098.java
@@ -0,0 +1,48 @@
+public class URI1098 {
+    public static void main(String[] args) {
+        for (int j = 1; j <= 3; j += 1) {
+            int i = 0;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 1.2; j <= 3.2; j += 1) {
+            double i = 0.2;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 1.4; j <= 3.4; j += 1) {
+            double i = 0.4;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 1.6; j <= 3.6; j += 1) {
+            double i = 0.6;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 1.8; j <= 3.8; j += 1) {
+            double i = 0.8;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (int j = 2; j <= 4; j += 1) {
+            int i = 1;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 2.2; j <= 4.2; j += 1) {
+            double i = 1.2;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 2.4; j <= 4.4; j += 1) {
+            double i = 1.4;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 2.6; j <= 4.6; j += 1) {
+            double i = 1.6;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (double j = 2.8; j <= 4.8; j += 1) {
+            double i = 1.8;
+            System.out.println("I=" + i + " J=" + j);
+        }
+        for (int j = 3; j <= 5; j += 1) {
+            int i = 2;
+            System.out.println("I=" + i + " J=" + j);
+        }
+    }
+}
