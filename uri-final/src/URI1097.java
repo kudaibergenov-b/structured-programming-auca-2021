509bb0251a75eed73241355f0975194f514fc86e
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-28 21:57:49 +0600

    uri-final URI1097 Accepted

---
+++ uri-final/src/URI1097.java
@@ -0,0 +1,29 @@
+public class URI1097 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 3; i += 2) {
+            for (int j = 7; j >= 5; j -= 1) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+        for (int i = 3; i < 5; i += 2){
+            for (int j = 9; j >= 7; j -= 1){
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+        for (int i = 5; i < 7; i += 2){
+            for (int j = 11; j >= 9; j -= 1){
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+        for (int i = 7; i < 9; i += 2){
+            for (int j = 13; j >= 11; j -= 1){
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+        for (int i = 9; i < 11; i += 2){
+            for (int j = 15; j >= 13; j -= 1){
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+    }
+}
