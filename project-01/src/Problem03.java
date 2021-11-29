7736a872d2fb0c47730705e1e1e7e93681be76b9
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-11-29 15:04:22 +0600

    project-01 Problem03 Done

--- project-01/src/Problem03.java
+++ project-01/src/Problem03.java
@@ -1,9 +1,9 @@
 import processing.core.*;
 
 public class Problem03 extends PApplet {
-    public static float RECT_WIDTH = 400;
+    public static float RECT_WIDTH = 550;
 
-    public static float RECT_HEIGHT = 150;
+    public static float RECT_HEIGHT = 100;
 
     public void settings() {
         fullScreen();
@@ -24,42 +24,42 @@ public class Problem03 extends PApplet {
         fill(0, 0, 255);
         rect(rectTopLeftX, rectTopLeftY, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(255, 255, 0);
-        text("Item 1", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT/ 2f);
+        fill(255, 255, 255);
+        text("Java Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT/ 2f);
 
         fill(0, 0, 255);
         rect(rectTopLeftX, rectTopLeftY + RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(255, 255, 0);
-        text("Item 2", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 3 / 2);
+        fill(255, 255, 255);
+        text("Kotlin Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 3 / 2);
 
         fill(0, 0, 255);
         rect(rectTopLeftX, rectTopLeftY + 2 * RECT_HEIGHT, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(255, 255, 0);
-        text("Item 3", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 5 / 2);
+        fill(255, 255, 255);
+        text("Scala Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 5 / 2);
 
-        fill(0, 0, 100);
-        ellipse(mouseX, mouseY, 100, 100);
-
-        fill(255, 255, 0);
-        text("mouseX: " + mouseX, width / 2f, height / 2f);
-        text("mouseY: " + mouseY, width / 2f, height / 2f + height / 30f);
 
         if (rectTopLeftX <= mouseX && mouseX <= rectTopLeftX + RECT_WIDTH &&
         rectTopLeftY <= mouseY && mouseY <= rectTopLeftY + RECT_HEIGHT) {
-            fill (255, 255, 255);
-            text("Item 1", width / 2f, height - height / 10f);
+            fill(255, 0, 0);
+            text("Java Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT/ 2f);
+            fill (255, 255, 0);
+            text("Year: 1995.PYPL Index: 2", width / 2f, height - height / 10f);
         }
         if (rectTopLeftX <= mouseX && mouseX <= rectTopLeftX + RECT_WIDTH &&
                 rectTopLeftY + RECT_HEIGHT <= mouseY && mouseY <= rectTopLeftY + 2 * RECT_HEIGHT) {
-            fill (255, 255, 255);
-            text("Item 2", width / 2f, height - height / 10f);
+            fill(255, 0, 0);
+            text("Kotlin Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 3 / 2);
+            fill (255, 255, 0);
+            text("Year: 2011.PYPL Index: 11", width / 2f, height - height / 10f);
         }
         if (rectTopLeftX <= mouseX && mouseX <= rectTopLeftX + RECT_WIDTH &&
                 rectTopLeftY + 2 * RECT_HEIGHT <= mouseY && mouseY <= rectTopLeftY + 3 * RECT_HEIGHT) {
-            fill (255, 255, 255);
-            text("Item 3", width / 2f, height - height / 10f);
+            fill(255, 0, 0);
+            text("Scala Programming Language", rectTopLeftX + RECT_WIDTH / 2f, rectTopLeftY + RECT_HEIGHT * 5 / 2);
+            fill (255, 255, 0);
+            text("Year 2004.PYPL Index: 20", width / 2f, height - height / 10f);
         }
     }
 
