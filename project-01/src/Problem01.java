c8e831d0d5018450358e20e86f36b0003583a938
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-01 15:46:58 +0600

    project-01 Problem01 changing only 2 colors

--- project-01/src/Problem01.java
+++ project-01/src/Problem01.java
@@ -16,20 +16,54 @@ public class Problem01 extends PApplet {
     public void setup() {
         messageSize = MIN_SIZE;
     }
+    int period = 1;
 
     public void draw() {
 
         background(0, 0, 0);
-        textSize(messageSize);
-        textAlign(CENTER, CENTER);
-        fill(255, 0, 0);
-        text(MESSAGE,width / 2f, height / 2f);
 
-        if (messageSize >= MAX_SIZE) {
-            DELTA = -1;
-        } else if (messageSize <= MIN_SIZE) {
-            DELTA = 1;
+        if (period == 1) {
+            textSize(messageSize);
+            textAlign(CENTER, CENTER);
+            fill(255, 0, 0);
+            text(MESSAGE, width / 2f, height / 2f);
+
+            if (messageSize > MAX_SIZE) {
+                DELTA = -1;
+            } else if (messageSize < MIN_SIZE) {
+                DELTA = 1;
+                period += 1;
+            }
         }
+        if (period == 2) {
+            textSize(messageSize);
+            textAlign(CENTER, CENTER);
+            fill(0, 255, 0);
+            text(MESSAGE, width / 2f, height / 2f);
+
+            if (messageSize > MAX_SIZE) {
+                DELTA = -1;
+            } else if (messageSize < MIN_SIZE) {
+                DELTA = 1;
+
+            }
+        }
+        if (period == 3) {
+            textSize(messageSize);
+            textAlign(CENTER, CENTER);
+            fill(0, 0, 255);
+            text(MESSAGE, width / 2f, height / 2f);
+
+            if (messageSize > MAX_SIZE) {
+                DELTA = -1;
+            } else if (messageSize < MIN_SIZE) {
+                DELTA = 1;
+            }
+        }
+
+
+
+
         messageSize += DELTA;
     }
 
