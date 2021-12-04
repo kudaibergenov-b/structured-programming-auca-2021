b2728f8f908ec9d639cb0c4061dcfc607206d2a2
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-04 17:08:11 +0600

    uri-final URI1114 Runtime error

---
+++ uri-final/src/URI1114.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1114 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+
+        while (inp.nextInt() != 2002) {
+            System.out.println("Senha Invalida");
+            }
+        System.out.println("Acesso Permitido");
+        System.exit(1);
+    }
+}
