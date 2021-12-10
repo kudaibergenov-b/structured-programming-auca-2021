9b91c38b7d130364712d4f16ef52d12068dd8069
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-10 21:01:55 +0600

    project-01 Problem02 work in progress (trying to understand how to locate other 2 circles)

--- project-01/src/Problem02.java
+++ project-01/src/Problem02.java
@@ -5,10 +5,13 @@ public class Problem02 extends PApplet {
     float y;
     float dx = random(-10, 10);
     float dy = random(-8, 8);
+    float randomPosX = random(-50, 50);
+    float randomPosY = 50 - abs(randomPosX);
 
     public void settings() {
         fullScreen();
     }
+
 
     public void setup() {
         x = 640;
@@ -17,10 +20,16 @@ public class Problem02 extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
+
         circle(x, y, 50 );
         fill(0, 0, 255);
+
+        circle(x + 50,y + 30, 50);
+        fill(255, 0, 0);
+
         x += dx;
         y += dy;
+
         if (x >= width) {
             x = width;
             dx = -dx;
