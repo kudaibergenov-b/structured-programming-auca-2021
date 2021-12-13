b9ab4073f33b5cdf37272b972d483f6b3e338383
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-13 13:19:32 +0600

    lab-06 Problem05 Completed

---
+++ lab-06/src/Problem05.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        try {
+            System.out.print("A = ");
+            long a = inp.nextLong();
+
+            System.out.print("B = ");
+            long b = inp.nextLong();
+
+            System.out.printf("GCD(%d,%d) = %d%n", a, b, gcd(a, b));
+        } catch (InputMismatchException e) {
+            System.out.println("It is not a correct int value");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    static long gcd(long a, long b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 && b == 0) {
+            throw new IllegalArgumentException("GCD(0, 0 is not defined");
+        }
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+        while (b != 0) {
+            long rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        return a;
+    }
+}
