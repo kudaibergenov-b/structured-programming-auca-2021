9fcf7f0a3ba5cf6df238a5e635db7d73d74a3d6e
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-23 22:16:25 +0600

    project-01 Problem04 Work in progress

--- project-01/src/Problem04.java
+++ project-01/src/Problem04.java
@@ -4,6 +4,10 @@ public class Problem04 extends PApplet {
     float circleX;
     float circleY;
     float circleR;
+    float horizontalX;
+    float horizontalY;
+    float verticalX;
+    float verticalY;
 
     public void settings() {
         fullScreen();
@@ -13,13 +17,24 @@ public class Problem04 extends PApplet {
         circleX = width / 2f;
         circleY = height / 2f;
         circleR = height / 20f;
+        horizontalX = width / 5f;
+        horizontalY = height / 8f;
         frameRate(10);
     }
 
     public void draw() {
+        noStroke();
         fill(0,0,0,50);
         rect(0, 0, width, height);
 
+        for (int i = 0; i < 21; ++i) {
+            stroke(0,0,255);
+            strokeWeight(2);
+            line(horizontalX, horizontalY, width - horizontalX, horizontalY);
+            horizontalY += circleR;
+        }
+
+        noStroke();
         fill(255, 0, 0);
         circle(circleX, circleY, circleR);
 
