2d6ba085258885b28868c7d6432d3d204b5558b2
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2021-12-24 00:12:52 +0600

    project-01 Problem04 Work in progress

--- project-01/src/Problem04.java
+++ project-01/src/Problem04.java
@@ -3,6 +3,8 @@ import processing.core.*;
 public class Problem04 extends PApplet {
     float circleX;
     float circleY;
+    float circleX2;
+    float circleY2;
     float circleR;
     float horizontalX;
     float horizontalY;
@@ -14,13 +16,15 @@ public class Problem04 extends PApplet {
     }
 
     public void setup() {
-        circleX = width / 2f;
-        circleY = height / 2f;
         circleR = height / 20f;
         horizontalX = width / 5f;
         horizontalY = height / 8f;
+        circleX = horizontalX + (circleR / 2);
+        circleY = horizontalY + (circleR / 2);
         verticalX = horizontalX;
         verticalY = horizontalY;
+        circleX2 = (int) random(circleX, circleX + circleR * 5);
+        circleY2 = (int) random(circleY, circleY);
         frameRate(10);
     }
 
@@ -29,23 +33,24 @@ public class Problem04 extends PApplet {
         fill(0,0,0, 50);
         rect(0,0, width, height);
 
-        for (int i = 0; i < 16; ++i) {
+        for (int i = 0, h = height / 8; i < 16; ++i, h += circleR) {
             stroke(0,0,255);
             strokeWeight(2);
-            line(horizontalX, horizontalY, width - horizontalX, horizontalY);
-            horizontalY += circleR;
+            line(horizontalX, h, horizontalX + circleR * 21, h);
         }
-        for (int j = 0; j < 21; ++j) {
+        for (int j = 0, v = width / 5; j < 21; ++j, v += circleR * 1.067) {
             stroke(0,0,255);
             strokeWeight(2);
-            line(verticalX, verticalY, verticalX, height - verticalY);
-            verticalX += circleR * 1.067;
+            line(v, verticalY, v, height - verticalY);
         }
-
 
         noStroke();
         fill(255, 0, 0);
         circle(circleX, circleY, circleR);
+
+        noStroke();
+        fill(255, 255, 0);
+        circle(circleX2, circleY2, circleR);
 
         if (key == CODED) {
             switch (keyCode) {
